7c97c9da61311e18db021586da647ed4fd58e94f
Richard Ferreira Salviano <richard.salviano@academico.ifpb.edu.br>
2023-03-23 17:02:11 -0300

    Create testebench.sv

---
+++ PrimeirosProjetos/HelloWorld/testebench.sv
@@ -0,0 +1,10 @@
+module hello();
+  
+	initial begin //usa quando está descrevendo o testebanch
+		$display("*** Hello World ***"); //imprime string na tela
+      
+    $finish(); //para a simulação
+      
+	end
+  
+endmodule: hello;
