c259ab12cd9ba2b37d37fa36e3745462e3875f2f
Richard Ferreira Salviano <richard.salviano@academico.ifpb.edu.br>
2023-05-19 11:29:28 -0300

    Update Contador.sv

--- projetosQuartus/contador8bits_assincrono/Contador.sv
+++ projetosQuartus/contador8bits_assincrono/Contador.sv
@@ -1,8 +1,15 @@
+/*
+ * Projeto: Circuito contador de 8 bits
+ * Descrição: Fazer um circuito contador com 8 saídas, assincrono up/down, para simular na fpga
+ * Autores: Patricia Santos e Richard Ferreira
+ * Data: 19/05/2023
+*/
+//criando modulo contador
 module contador(
   input clk, rst, ud,
   output [7:0] count
   );
-    
+   
   always_ff @(posedge clk or posedge rst) 
     begin
       if(rst) 
@@ -15,20 +22,20 @@ module contador(
           0:
             begin
             	count <= count - 1; //decrementa
-              	$display("Saída =\n   Decimal: %d\n   Binario: %b", cont, cont);
+		$display("Saída =\n   Decimal: %d\n   Binario: %b", count, count);
           end
           1:
             begin
             	count <= count + 1; //incrementa
-              	$display("Saída =\n   Decimal: %d\n   Binario: %b", cont, cont);
+		$display("Saída =\n   Decimal: %d\n   Binario: %b", count, count);
           end 
-			 default:
-				count <= 8'b0; //Reinicia do 0
+	     default:
+		count <= 8'b0; //Reinicia do 0
         endcase
       end
   
 endmodule: contador
-
+//Criando o modulo de divisor de frequencia 
 module divfreq(clk_in, rst, clk_out);
          
   input clk_in, rst;
@@ -43,6 +50,7 @@ module divfreq(clk_in, rst, clk_out);
         clk_out = 0;
       end
       else
+	//configurando para 5hz 
         if (count == 5000000 ) begin
             count = 0;
             clk_out = ~clk_out;
